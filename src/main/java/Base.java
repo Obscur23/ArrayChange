32326cae0bb879c3201cc43def09bba07378fc5e
Obscur23 <obscuredo@mail.ru>
2017-12-06 23:48:49 +0300

    Rabotaet

---
+++ src/main/java/Base.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * Created by Obscur on 06.12.17.
+ */
+public class Base {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Integer[] aRRay = new Integer[20];
+        //Инициализация массива
+        for (int i = 0; i<20; i++ ) {
+            aRRay[i] = -10 + (int) (Math.random() * 20);
+            System.out.println("Array " + (i) + " is " + aRRay[i]);
+            //Заполнение массива
+        }
+        int ma0 = -1;
+        int miP = 10;
+        int miPNUM = 0;
+        int maONUM = 0;
+        for (int i = 0; i<20; i++ ){ //Запускаем прогон по массиву
+            if (aRRay[i] > 0){ // Если число +, ищем минимальное
+                if (aRRay[i] < miP){
+                    miP = aRRay[i];
+                    miPNUM = i;} //запоминаем номер в массиве
+                }
+            if (aRRay[i] < 0){ // Если число -, ищем максимальное
+                if (aRRay[i] < ma0){
+                    ma0 = aRRay[i];
+                    maONUM = i;} //запоминаем номер в массиве
+            }
+        }
+        System.out.println(maONUM+"Max- = "+aRRay[maONUM]+miPNUM+"  Min+ = "+aRRay[miPNUM]);
+    }
+}
